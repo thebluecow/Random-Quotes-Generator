375ecb7b90262983216ed99ec7f5881a04699d45
scott bline <thebluecow@msn.com>
2016-12-29 15:31:01 -0500

    Add viewed property
    Added viewed property to show how many times a particular quote has
    been viewed in one session. Logs information to console.

--- js/quotes.js
+++ js/quotes.js
@@ -1,7 +1,8 @@
-// JavaScript Document
-
 /*
   quotes array is used to store quote objects.
+
+  Note: The quotes.js file is no longer used but is reserved in case it will be used
+  in the future.
 */
 var quotes = [
   {
@@ -10,7 +11,8 @@ var quotes = [
     citation: "GI Joe file card",
     year: "1983",
     tags: "toys, gi joe, fun",
-    character: "Cobra Commander"
+    character: "Cobra Commander",
+    viewed: 0
   },
   {
     quote: "Charlie is a Shaman, a medicine man. He's not a healer or a priest or a witch-doctor. There isn't any equivalent in our culture for what he is unless we had shrinks that could actually help people.",
@@ -18,14 +20,16 @@ var quotes = [
     citation: "GI Joe file card",
     year: "1984",
     tags: "toys, gi joe, spirit",
-    character: "Spirit"
+    character: "Spirit",
+    viewed: 0
   },
   {
     quote: "The harder the world, the fiercer the honour.",
     source: "Steven Erickson",
     citation: "Memories of Ice",
     year: "2001",
-    tags: "malazan, fantasy, novels"
+    tags: "malazan, fantasy, novels",
+    viewed: 0
   },
   {
     quote: "I've come to understand that the truth isn't really what's important...what really matters is what people agree is the truth.",
@@ -33,21 +37,24 @@ var quotes = [
     citation: "Stonewielder",
     year: "2010",
     tags: "malazan, fantasy, novels",
-    character: "Greymane"
+    character: "Greymane",
+    viewed: 0
   },
   {
     quote: "Chaos needs no allies, for it dwells like a poison in every one of us.",
     source: "Steven Erickson",
     citation: "Midnight Tides",
     year: "2004",
-    tags: "malazan, fantasy, poison"
+    tags: "malazan, fantasy, poison",
+    viewed: 0
   },
   {
     quote: "What makes a Malazan soldier so dangerous? They’re allowed to think.",
     source: "Steven Erickson",
     citation: "Deadhouse Gates",
     year: "2000",
-    tags: "malazan, fantasy, dangerous"
+    tags: "malazan, fantasy, dangerous",
+    viewed: 0
   },
   {
     quote: "So we beat on, boats against the current, borne back ceaselessly into the past.",
@@ -55,7 +62,8 @@ var quotes = [
     citation: "The Great Gatsby",
     year: "1925",
     tags: "literature, great, nick",
-    character: "Nick Carraway"
+    character: "Nick Carraway",
+    viewed: 0
   },
   {
     quote: "Every one suspects himself of at least one of the cardinal virtues, and this is mine: I am one of the few honest people that I have ever known.",
@@ -63,7 +71,8 @@ var quotes = [
     citation: "The Great Gatsby",
     year: "1925",
     tags: "literature, great, nick",
-    character: "Nick Carraway"
+    character: "Nick Carraway",
+    viewed: 0
   },
   {
     quote: "A pain stabbed my heart, as it did every time I saw a girl I loved who was going the opposite direction in this too-big world.",
@@ -71,42 +80,49 @@ var quotes = [
     citation: "On the Road",
     year: "1957",
     tags: "kerouac, beat, literature",
-    character: "Sal Paradise"
+    character: "Sal Paradise",
+    viewed: 0
   },
   {
     quote: "A friend is one who walks in when the rest of the world walks out.",
     source: "Anonymous",
-    tags: "anonymous, friend, world"
+    tags: "anonymous, friend, world",
+    viewed: 0
   },
   {
     quote: "Contentment is not the fulfillment of what you want, but the realization of how much you already have.",
     source: "Anonymous",
-    tags: "anonymous, friend, world"
+    tags: "anonymous, friend, world",
+    viewed: 0
   },
   {
     quote: "If you really do put a small value upon yourself, rest assured that the world will not raise your price.",
     source: "Anonymous",
-    tags: "anonymous, friend, world"
+    tags: "anonymous, friend, world",
+    viewed: 0
   },
   {
     quote: "Remember, a Cobra is a snake and snake is sneak spelled sideways!",
     source: "A Real American Hero",
     citation: "GI Joe cartoon",
     tags: "gi joe, flint, cartoon",
-    character: "Flint"
+    character: "Flint",
+    viewed: 0
   },
   {
     quote: "Give your hearts to America, Joes, 'cause your butts belong to me!",
     source: "A Real American Hero",
     citation: "GI Joe cartoon",
     tags: "gi joe, slaughter, cartoon",
-    character: "Sgt. Slaughter"
+    character: "Sgt. Slaughter",
+    viewed: 0
   },
   {
     quote: "Let's reconnoiter, Snake Eyes, and try not to attract attention. Sure. Who'd notice a wet sailor with a parrot and a silent, masked man with a timber wolf? I think we're in major trouble!",
     source: "A Real American Hero",
     citation: "GI Joe cartoon",
     tags: "gi joe, shipwreck, cartoon",
-    character: "Shipwreck"
+    character: "Shipwreck",
+    viewed: 0
   }
 ];
--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
 /*
   quotes array is used to store quote objects.
 */
@@ -11,7 +12,8 @@ var quotes = [
     citation: "GI Joe file card",
     year: "1983",
     tags: "toys, gi joe, fun",
-    character: "Cobra Commander"
+    character: "Cobra Commander",
+    viewed: 0
   },
   {
     quote: "Charlie is a Shaman, a medicine man. He's not a healer or a priest or a witch-doctor. There isn't any equivalent in our culture for what he is unless we had shrinks that could actually help people.",
@@ -19,14 +21,16 @@ var quotes = [
     citation: "GI Joe file card",
     year: "1984",
     tags: "toys, gi joe, spirit",
-    character: "Spirit"
+    character: "Spirit",
+    viewed: 0
   },
   {
     quote: "The harder the world, the fiercer the honour.",
     source: "Steven Erickson",
     citation: "Memories of Ice",
     year: "2001",
-    tags: "malazan, fantasy, novels"
+    tags: "malazan, fantasy, novels",
+    viewed: 0
   },
   {
     quote: "I've come to understand that the truth isn't really what's important...what really matters is what people agree is the truth.",
@@ -34,21 +38,24 @@ var quotes = [
     citation: "Stonewielder",
     year: "2010",
     tags: "malazan, fantasy, novels",
-    character: "Greymane"
+    character: "Greymane",
+    viewed: 0
   },
   {
     quote: "Chaos needs no allies, for it dwells like a poison in every one of us.",
     source: "Steven Erickson",
     citation: "Midnight Tides",
     year: "2004",
-    tags: "malazan, fantasy, poison"
+    tags: "malazan, fantasy, poison",
+    viewed: 0
   },
   {
     quote: "What makes a Malazan soldier so dangerous? They’re allowed to think.",
     source: "Steven Erickson",
     citation: "Deadhouse Gates",
     year: "2000",
-    tags: "malazan, fantasy, dangerous"
+    tags: "malazan, fantasy, dangerous",
+    viewed: 0
   },
   {
     quote: "So we beat on, boats against the current, borne back ceaselessly into the past.",
@@ -56,7 +63,8 @@ var quotes = [
     citation: "The Great Gatsby",
     year: "1925",
     tags: "literature, great, nick",
-    character: "Nick Carraway"
+    character: "Nick Carraway",
+    viewed: 0
   },
   {
     quote: "Every one suspects himself of at least one of the cardinal virtues, and this is mine: I am one of the few honest people that I have ever known.",
@@ -64,7 +72,8 @@ var quotes = [
     citation: "The Great Gatsby",
     year: "1925",
     tags: "literature, great, nick",
-    character: "Nick Carraway"
+    character: "Nick Carraway",
+    viewed: 0
   },
   {
     quote: "A pain stabbed my heart, as it did every time I saw a girl I loved who was going the opposite direction in this too-big world.",
@@ -72,46 +81,54 @@ var quotes = [
     citation: "On the Road",
     year: "1957",
     tags: "kerouac, beat, literature",
-    character: "Sal Paradise"
+    character: "Sal Paradise",
+    viewed: 0
   },
   {
     quote: "A friend is one who walks in when the rest of the world walks out.",
     source: "Anonymous",
-    tags: "anonymous, friend, world"
+    tags: "anonymous, friend, world",
+    viewed: 0
   },
   {
     quote: "Contentment is not the fulfillment of what you want, but the realization of how much you already have.",
     source: "Anonymous",
-    tags: "anonymous, friend, world"
+    tags: "anonymous, friend, world",
+    viewed: 0
   },
   {
     quote: "If you really do put a small value upon yourself, rest assured that the world will not raise your price.",
     source: "Anonymous",
-    tags: "anonymous, friend, world"
+    tags: "anonymous, friend, world",
+    viewed: 0
   },
   {
     quote: "Remember, a Cobra is a snake and snake is sneak spelled sideways!",
     source: "A Real American Hero",
     citation: "GI Joe cartoon",
     tags: "gi joe, flint, cartoon",
-    character: "Flint"
+    character: "Flint",
+    viewed: 0
   },
   {
     quote: "Give your hearts to America, Joes, 'cause your butts belong to me!",
     source: "A Real American Hero",
     citation: "GI Joe cartoon",
     tags: "gi joe, slaughter, cartoon",
-    character: "Sgt. Slaughter"
+    character: "Sgt. Slaughter",
+    viewed: 0
   },
   {
     quote: "Let's reconnoiter, Snake Eyes, and try not to attract attention. Sure. Who'd notice a wet sailor with a parrot and a silent, masked man with a timber wolf? I think we're in major trouble!",
     source: "A Real American Hero",
     citation: "GI Joe cartoon",
     tags: "gi joe, shipwreck, cartoon",
-    character: "Shipwreck"
+    character: "Shipwreck",
+    viewed: 0
   }
 ];
 
+// globally scoped variable to hold "viewed" quotes
 var quotesUsed = [];
 
 function getRandomNumber(max) {
@@ -143,10 +160,14 @@ function getRandomQuote() {
     randomNumber = getRandomNumber(quotes.length);
     quote = quotes[randomNumber];
 
+    // Add 1 to the viewed property (in case this will be used in the future)
+    quote.viewed += 1;
+
     // to ensure unique quotes, remove the quote from the quotes array
     // and place in the quotesUsed array
     quotes.splice(randomNumber, 1);
     quotesUsed.push(quote);
+
   } else {
     // copy all the entries in the quotesUsed array to quotes.
     quotes = quotesUsed.slice(0);
@@ -188,21 +209,23 @@ function printQuote() {
   htmlString += '<p class="source">' + quote.source;
 
   // write quote to console
-  console.log(quote.quote, "remaining quotes: " + quotes.length + ". The array will reset at 0.");
+  console.log(quote.quote, "remaining quotes: " + quotes.length, "this quote has been viewed " + quote.viewed + " total time(s).");
 
   // loops through the properties array to add additional information to
   // the htmlString.
   for (var i = 0; i < properties.length; i++) {
     var property = properties[i];
 
-    if (property !== 'quote' && property !== 'source') {
+    if (property !== 'quote' && property !== 'source' && property !== 'viewed') {
       // add each property to the htmlString
       htmlString += '<span class="' + property + '">' + quote[property] + '</span>';
       }
   }
 
   // change the background color each time a quote is generated
-  document.body.style.backgroundColor = getRandomColor();
+  var randomColor = getRandomColor();
+  document.body.style.backgroundColor = randomColor;
+  document.getElementById('loadQuote').style.backgroundColor = randomColor;
 
   // sets the div#quote-box with the htmlString
   document.getElementById('quote-box').innerHTML = htmlString;
