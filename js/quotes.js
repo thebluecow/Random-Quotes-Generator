8032101ed5499f28531f5e926e7b196483d36431
scott bline <thebluecow@msn.com>
2016-12-29 12:59:25 -0500

    Completed Initial Steps
    Achieved goals first established before attempting to earn additional
    points.

---
+++ js/quotes.js
@@ -0,0 +1,112 @@
+// JavaScript Document
+
+/*
+  quotes array is used to store quote objects.
+*/
+var quotes = [
+  {
+    quote: "COBRA Commander is hatred and evil personified. Corrupt. A man without scruples. Probably the most dangerous man alive!",
+    source: "Anonymous",
+    citation: "GI Joe file card",
+    year: "1983",
+    tags: "toys, gi joe, fun",
+    character: "Cobra Commander"
+  },
+  {
+    quote: "Charlie is a Shaman, a medicine man. He's not a healer or a priest or a witch-doctor. There isn't any equivalent in our culture for what he is unless we had shrinks that could actually help people.",
+    source: "Anonymous",
+    citation: "GI Joe file card",
+    year: "1984",
+    tags: "toys, gi joe, spirit",
+    character: "Spirit"
+  },
+  {
+    quote: "The harder the world, the fiercer the honour.",
+    source: "Steven Erickson",
+    citation: "Memories of Ice",
+    year: "2001",
+    tags: "malazan, fantasy, novels"
+  },
+  {
+    quote: "I've come to understand that the truth isn't really what's important...what really matters is what people agree is the truth.",
+    source: "Ian C. Esslemont",
+    citation: "Stonewielder",
+    year: "2010",
+    tags: "malazan, fantasy, novels",
+    character: "Greymane"
+  },
+  {
+    quote: "Chaos needs no allies, for it dwells like a poison in every one of us.",
+    source: "Steven Erickson",
+    citation: "Midnight Tides",
+    year: "2004",
+    tags: "malazan, fantasy, poison"
+  },
+  {
+    quote: "What makes a Malazan soldier so dangerous? They’re allowed to think.",
+    source: "Steven Erickson",
+    citation: "Deadhouse Gates",
+    year: "2000",
+    tags: "malazan, fantasy, dangerous"
+  },
+  {
+    quote: "So we beat on, boats against the current, borne back ceaselessly into the past.",
+    source: "F. Scott Fitzgerald",
+    citation: "The Great Gatsby",
+    year: "1925",
+    tags: "literature, great, nick",
+    character: "Nick Carraway"
+  },
+  {
+    quote: "Every one suspects himself of at least one of the cardinal virtues, and this is mine: I am one of the few honest people that I have ever known.",
+    source: "F. Scott Fitzgerald",
+    citation: "The Great Gatsby",
+    year: "1925",
+    tags: "literature, great, nick",
+    character: "Nick Carraway"
+  },
+  {
+    quote: "A pain stabbed my heart, as it did every time I saw a girl I loved who was going the opposite direction in this too-big world.",
+    source: "Jack Kerouac",
+    citation: "On the Road",
+    year: "1957",
+    tags: "kerouac, beat, literature",
+    character: "Sal Paradise"
+  },
+  {
+    quote: "A friend is one who walks in when the rest of the world walks out.",
+    source: "Anonymous",
+    tags: "anonymous, friend, world"
+  },
+  {
+    quote: "Contentment is not the fulfillment of what you want, but the realization of how much you already have.",
+    source: "Anonymous",
+    tags: "anonymous, friend, world"
+  },
+  {
+    quote: "If you really do put a small value upon yourself, rest assured that the world will not raise your price.",
+    source: "Anonymous",
+    tags: "anonymous, friend, world"
+  },
+  {
+    quote: "Remember, a Cobra is a snake and snake is sneak spelled sideways!",
+    source: "A Real American Hero",
+    citation: "GI Joe cartoon",
+    tags: "gi joe, flint, cartoon",
+    character: "Flint"
+  },
+  {
+    quote: "Give your hearts to America, Joes, 'cause your butts belong to me!",
+    source: "A Real American Hero",
+    citation: "GI Joe cartoon",
+    tags: "gi joe, slaughter, cartoon",
+    character: "Sgt. Slaughter"
+  },
+  {
+    quote: "Let's reconnoiter, Snake Eyes, and try not to attract attention. Sure. Who'd notice a wet sailor with a parrot and a silent, masked man with a timber wolf? I think we're in major trouble!",
+    source: "A Real American Hero",
+    citation: "GI Joe cartoon",
+    tags: "gi joe, shipwreck, cartoon",
+    character: "Shipwreck"
+  }
+];
--- js/script.js
+++ js/script.js
@@ -2,3 +2,56 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+// retrieves a random record from the quotes array contained in the
+// js/quotes.js file
+// returns a quote object
+function getRandomQuote() {
+  var randomNumber = Math.floor(Math.random() * quotes.length);
+  var quote = quotes[randomNumber];
+
+  return quote;
+};
+
+function sortProperties(quote) {
+  // variable to store the properties that exist in the quote object
+  var properties = Object.getOwnPropertyNames(quote);
+
+  // used to ensure the tags property is always last
+  // did this in case new properties were added to the objects in the future
+  // such as boolean for already shown
+  for (var x in properties)properties[x] === "tags" ? properties.push(properties.splice(x,1)[0]) : 0;
+
+  return properties;
+
+}
+
+// constructs the HTML formatted string to be displayed on the index.html page
+function printQuote() {
+  // variable to store the returned quote object
+  var quote = getRandomQuote();
+
+  // create and initialize the string
+  var htmlString = "";
+
+  var properties = sortProperties(quote);
+
+  // add the quote and source to the htmlString since these will always
+  // be present for each quote
+  htmlString += '<p class="quote">' + quote.quote + '</p>';
+  htmlString += '<p class="source">' + quote.source;
+
+  // loops through the properties array to add additional information to
+  // the htmlString.
+  for (var i = 0; i < properties.length; i++) {
+    var property = properties[i];
+
+    if (property !== 'quote' && property !== 'source') {
+      // add each property to the htmlString
+      htmlString += '<span class="' + property + '">' + quote[property] + '</span>';
+      }
+  }
+
+  // sets the div#quote-box with the htmlString
+  document.getElementById('quote-box').innerHTML = htmlString;
+
+};
